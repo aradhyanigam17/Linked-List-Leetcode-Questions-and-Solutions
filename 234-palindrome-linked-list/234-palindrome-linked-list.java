c46a7ab2d2b2ad11abff347762ba42f9b6378027
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-03-10 18:16:14 +0530

    Time: 5 ms (90.02%), Space: 97.4 MB (61.53%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,23 +11,27 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head;
-	ListNode fast = head;
-	ListNode prev = null;
-	while (fast != null && fast.next != null) {
-		fast = fast.next.next;
-		ListNode temp = slow;
-		slow = slow.next;
-		temp.next = prev;
-		prev = temp;
-	}
-	//Handling for odd length linked list
-	if (fast != null) slow = slow.next;
-	while (prev != null && slow != null) {
-		if (prev.val != slow.val) return false;
-		prev = prev.next;
-		slow = slow.next;
-	}
-	return prev == null && slow == null;
+        ListNode fast = head;
+        ListNode prev = null;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode temp = slow;
+            slow = slow.next;
+            temp.next = prev;
+            prev = temp;
+        }
+        
+        //Handling for odd length linked list
+        if (fast != null) slow = slow.next;
+
+            while (prev != null && slow != null) {
+                if (prev.val != slow.val) return false;
+                prev = prev.next;
+                slow = slow.next;
+        }
+
+        return true;
 
     }
 }
